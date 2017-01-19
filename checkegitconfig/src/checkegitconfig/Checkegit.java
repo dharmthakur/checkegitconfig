b0c6881ba32ff709c8cd6b9197db486fb1f46630
dharmendra <dharm1180@yahoo.co.in>
2017-01-19 20:58:38 +0530

    "chechegit again modified"

--- checkegitconfig/src/checkegitconfig/Checkegit.java
+++ checkegitconfig/src/checkegitconfig/Checkegit.java
@@ -5,5 +5,6 @@ public class Checkegit {
 	public static void main(String[] args){
 		System.out.println("this is to check eclipse with git hub");
 		System.out.println("dharmendra thakur");
+		System.out.println("hello");
 	}
 }
